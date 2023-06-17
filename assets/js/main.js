d40b3336e0d564464bf3b1219e696b8805d2ec25
diyorcodes <123626113+diyorcodes@users.noreply.github.com>
2023-06-17 10:03:30 +0500

    Js created a scroll header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+                        : header.classList.remove('scroll-header')                    
+}
+
+window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
