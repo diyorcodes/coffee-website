d80ea5f6c69882fd57b9ec2ecf2333c612a907ac
diyorcodes <123626113+diyorcodes@users.noreply.github.com>
2023-06-17 22:08:26 +0500

    Creating and styling the scroll part

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,14 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = ()  =>{
+    const scrollUp = document.getElementById('scroll-up')
 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                    : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
