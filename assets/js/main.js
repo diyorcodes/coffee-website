4dc65d98ddee6b43739b8ab30bdcafc07181d637
diyorcodes <123626113+diyorcodes@users.noreply.github.com>
2023-06-18 21:21:55 +0500

    Giving styles to active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,26 @@ const scrollUp = ()  =>{
 window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const section = document.querySelectorAll('section[id]')
 
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset
+
+    section.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }else{
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
