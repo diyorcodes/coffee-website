9b5ab2e2f74d3b3a68fd7d4c0d9df0e330edfbb6
diyorcodes <123626113+diyorcodes@users.noreply.github.com>
2023-06-20 19:38:13 +0500

    Media is being fed to the website

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,4 +42,17 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true  // Anomation repeat
+})
 
+sr.reveal(`.home__data, .products__data, .steps__content,
+            .footer__container`)
+sr.reveal(`.home__img`, {origin: 'bottom'})
+sr.reveal(`.products__card`, {interval: 100})
+sr.reveal(`.about__img, .testimonial__img`, {origin: 'right'})
+sr.reveal(`.about__data, .testimonial__data`, {origin: 'left'})
